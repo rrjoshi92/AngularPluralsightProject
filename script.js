ce41fa8fb376d140e79e3274248820fba79c8a4d
rrjoshi92 <rrjoshi92@gmail.com>
2016-02-21 06:37:39 -0800

    ng- repeat  code added

--- script.js
+++ script.js
@@ -5,14 +5,19 @@
 
   var MainController = function($scope,$http) {
   
-  var onUserComplete= function(response){
-    
-      $scope.user = response.data;     
+  var onUserComplete= function(response){    
+      $scope.user = response.data;
+      $http.get($scope.user.repos_url)
+          .then(onRepos, onError);
   };
   
   var onError = function(reason){
     $scope.error="Could not fatch the user";
   };
+
+  var onRepos = function (response) {
+      $scope.repos = response.data;
+  }
   
   $http.get("https://api.github.com/users/rrjoshi92")
   .then(onUserComplete,onError);
