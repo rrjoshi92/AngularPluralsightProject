3b2c1e161ba2e452d5e302324214979bb0d12cc4
rrjoshi92 <rrjoshi92@gmail.com>
2016-02-21 07:43:15 -0800

    services added

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
-//http://www.gravatar.com/avatar/4208631e04a8ee7754febcb498a854f8
+
 (function () {
 
-  var app = angular.module("githubViewer", []);
+    var app = angular.module("githubViewer", []);
 
-  var MainController = function($scope,$http) {
-  
-  var onUserComplete= function(response){    
-      $scope.user = response.data;
-      $http.get($scope.user.repos_url)
-          .then(onRepos, onError);
-  };
-  
-  var onError = function(reason){
-    $scope.error="Could not fatch the user";
-  };
+    var MainController = function ($scope, $http, $interval) {
 
-  var onRepos = function (response) {
-      $scope.repos = response.data;
-  }
-  
-  $http.get("https://api.github.com/users/rrjoshi92")
-  .then(onUserComplete,onError);
+        var onUserComplete = function (response) {
+            $scope.user = response.data;
+            $http.get($scope.user.repos_url)
+                .then(onRepos, onError);
+        };
 
-  $scope.search = function (username) {
-      $http.get("https://api.github.com/users/"+username)
-          .then(onUserComplete, onError)
-  }
-    
-  $scope.username = "angular";
-  $scope.message="GitHub Viewer";
-  };
+        var onError = function (reason) {
+            $scope.error = "Could not fatch the data";
+        };
 
-  app.controller("MainController",["$scope","$http", MainController]);
+        var onRepos = function (response) {
+            $scope.repos = response.data;
+        }
+
+
+        var decrementCountdown = function () {
+            $scope.countdown -= 1;
+            if ($scope.countdown < 1) {
+                $scope.search($scope.username);
+            }
+        }
+        var startcountdown = function() {
+            $interval(decrementCountdown,1000, $scope.countdown);
+        }
+
+        $scope.search = function (username) {
+            if (username) {
+                $http.get("https://api.github.com/users/" + username)
+                    .then(onUserComplete, onError)
+            }
+        }
+
+
+        $scope.message = "GitHub Viewer";
+        $scope.repoSortOrder = "-stargazers_count"
+        $scope.countdown = 5;
+        startcountdown();
+    };
+
+    app.controller("MainController", MainController);
 
 }());
