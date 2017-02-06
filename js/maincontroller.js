91a7aada18b91f43acd08940c43948b97f55e537
rrjoshi92 <rrjoshi92@gmail.com>
2017-02-05 23:28:07 -0800

    redirect fix

--- js/maincontroller.js
+++ js/maincontroller.js
@@ -3,13 +3,16 @@
 
     var app = angular.module("githubViewer");
 
-    var MainController = function ($scope, $location) {
+    var MainController = function ($scope, $location, $window) {
         $scope.search = function (username) {
            // if (username) {
                 // github.getuser(username);
                 $location.path("/angularProject/user/" + username);
            // }           
-        }        
+        }
+        $scope.goToPortfolio = function () {
+            $window.location.href = "http://ravi.dev-tech.club/portfolio/";
+        }
     };
 
     
